cde20db5db036d00e5121080842d0f67d37226d0
marcosDiFilippo <marcosjesusdifilippo@gmail.com>
2025-10-31 12:21:51 -0300

    agregamos un poco de js y mejoramos el estilo del alta

--- admin/pacientes.php
+++ admin/pacientes.php
@@ -5,12 +5,18 @@
 ?>
 <main id="main-pacientes">
     <section>
-        <article>
-            <button id="button-alta">Agregar Paciente</button>
+        <article id="article-buttons">
+            <button class="d-flex align-items-center" id="button-alta">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+                Agregar Paciente
+            </button>
+            <button id="cerrar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+            </button>
         </article>
     </section>
     <section id="section-alta-pacientes">
-        <article>
+        <article class="container" id="article-alta-pacientes"> 
             <?php
                 if (isset($_GET["alta"])) {
                     echo "<div class='alert alert-success' role='alert'>
@@ -23,77 +29,90 @@
                         </div>";
                 }
             ?>
-            <form id="form-alta" action="./abml/alta.php" method="post" enctype="multipart/form-data">
-                <div>
-                    <input type="text" name="nombre" id="nombre" placeholder="Ingresa el nombre">
-                    <input type="text" name="apellido" id="apellido" placeholder="Ingresa el apellido">
+            <form class="container-fluid" id="form-alta" action="./abml/alta.php" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <input class="col-6" type="text" name="nombre" id="nombre" placeholder="Ingresa el nombre">
+                    <input class="col-6" type="text" name="apellido" id="apellido" placeholder="Ingresa el apellido">
                 </div>
                 <div>
                     <label for="fecha-nacimiento">Fecha Nacimiento</label>
-                    <div>
-                        <input type="date" name="fecha-nacimiento" id="fecha-nacimiento">
-                        <input type="number" name="dni" id="dni" placeholder="Ingrese el dni">
+                    <div class="row"> 
+                        <input class="col-6" type="date" name="fecha-nacimiento" id="fecha-nacimiento">
+                        <input class="col-6" type="number" name="dni" id="dni" placeholder="Ingrese el dni">
                     </div>
                 </div>
-                <div>
-                    <input type="email" name="email" id="email" placeholder="Ingrese el email" autocomplete="additional-name">
-                    <input type="number" name="telefono" id="telefono" placeholder="Ingrese el telefono">
+                <div class="row">
+                    <input class="col-6" type="email" name="email" id="email" placeholder="Ingrese el email" autocomplete="additional-name">
+                    <input class="col-6" type="number" name="telefono" id="telefono" placeholder="Ingrese el telefono">
                 </div>
-                <div>   
-                    <label for="fecha">Fecha Consulta</label>
-                    <div>
+                <div class="row">   
+                    <div class="col-6 d-flex flex-column">
+                        <label for="fecha">Fecha Consulta</label>
                         <input type="date" name="fecha" id="fecha" placeholder="Ingrese fecha de la consulta">
+                    </div>
+                    <div class="col-6 d-flex flex-column"">
+                        <label for="hora">Horario</label>
                         <input type="time" name="hora" id="hora" placeholder="Ingrese horario de la consulta">
                     </div>
                 </div>
+                <hr>
                 <div>
-                    <label for="monto">Pago</label>
-                    <input type="number" name="monto" id="monto" placeholder="Ingrese el monto de la sesion">
-                    <div class="checkbox-metodos-pago">
-                        <?php
-                        $metodosPago = mysqli_query($conexion,$lecturaMetodosPago);
-                        
-                        while($metodoPago = mysqli_fetch_array($metodosPago)) {
-                            echo "<input type='checkbox' name='metodos-pago[]' id='$metodoPago[nombre]' value='$metodoPago[id_metodos_pago]'>";
-                            echo "<label class='label-checkbox' for='$metodoPago[nombre]'>$metodoPago[nombre]</label>";
-                        }
-                        ?>
-                    </div>
-                </div>
-                <label for="estado">Estado</label>
-                <select name="estado" id="estado">
-                    <?php
-                        $estados = mysqli_query($conexion,$lecturaEstados);
-                        
-                        while($estado = mysqli_fetch_array($estados)) {
-                            echo "<option value='$estado[id_estado]'>$estado[nombre]</option>";
-                        }
-                    ?>
-                    </select>
                     <div>
-                        <p class="motivo-consulta">Motivo Consulta</p>
-                        <div class="div-consulta">
+                        <div class="row d-flex align-items-center">
+                            <label class="col-2" for="monto">Pago</label>
+                            <input class="col-4" type="number" name="monto" id="monto" placeholder="Ingrese el monto de la sesion">
+                        </div>
                         <?php
-                            $tratamientos = mysqli_query($conexion,$lecturaTratamientos);
+                            $metodosPago = mysqli_query($conexion,$lecturaMetodosPago);
                             
-                            while($tratamiento = mysqli_fetch_array($tratamientos)) {
-                                echo "<div class='div-checkbox'>";
-                                echo "<input type='checkbox' name='tratamientos[]' id='$tratamiento[nombre]' value='$tratamiento[id_tratamientos]'>";
-                                echo "<label class='label-checkbox' for='$tratamiento[nombre]'>$tratamiento[nombre]</label>";
-                                echo "</div>";
+                            while($metodoPago = mysqli_fetch_array($metodosPago)) {
+                                echo "<input type='checkbox' name='metodos-pago[]' id='$metodoPago[nombre]' value='$metodoPago[id_metodos_pago]'>";
+                                echo "<label class='label-checkbox col-2' for='$metodoPago[nombre]'>$metodoPago[nombre]</label>";
                             }
                         ?>
                     </div>
                 </div>
+                <hr>
                 <div>
-                    <textarea name="detalles" id="detalles" rows="10" placeholder="Ingrese los detalles sobre la consulta (Opcional)"></textarea>
+                    <div class="col-6 >
+                        <label for="estado">Estado</label>
+                        <select name="estado" id="estado">
+                            <?php
+                                $estados = mysqli_query($conexion,$lecturaEstados);
+                                
+                                while($estado = mysqli_fetch_array($estados)) {
+                                    echo "<option value='$estado[id_estado]'>$estado[nombre]</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <hr>
+                <div>
+                    <p class="motivo-consulta">Motivo Consulta</p>
+                    <div class="row">
+                    <?php
+                        $tratamientos = mysqli_query($conexion,$lecturaTratamientos);
+                        
+                        while($tratamiento = mysqli_fetch_array($tratamientos)) {
+                            echo "<div class='div-checkbox col-2'>";
+                            echo "<input type='checkbox' name='tratamientos[]' id='$tratamiento[nombre]' value='$tratamiento[id_tratamientos]'>";
+                            echo "<label class='label-checkbox' for='$tratamiento[nombre]'>$tratamiento[nombre]</label>";
+                            echo "</div>";
+                        }
+                    ?>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <textarea class="col-12" name="detalles" id="detalles" rows="10" placeholder="Ingrese los detalles sobre la consulta (Opcional)"></textarea>
                 </div>
                 <div>
                     <label for="imagen">Ingrese la imagen relacionada a la sesion</label>
                     <input type="file" name="imagen" id="imagen" required>
                 </div>
                 <div>
-                    <button id="submit-alta" class="btn btn-primary" type="submit" value="Cargar Paciente">Cargar Paciente</button>
+                    <button id="submit-alta" class="btn btn-primary col-6" type="submit" value="Cargar Paciente">Cargar Paciente</button>
                 </div>
             </form>
         </article>
