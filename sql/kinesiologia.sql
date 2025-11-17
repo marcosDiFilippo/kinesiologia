679e5e3f1f44d174f33ffd4d2ccfc8fbe2b98815
marcosDiFilippo <marcosjesusdifilippo@gmail.com>
2025-11-17 19:49:15 -0300

    exportamos la base de datos y la gguardamos

--- sql/kinesiologia.sql
+++ sql/kinesiologia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 10-11-2025 a las 19:05:42
+-- Tiempo de generación: 17-11-2025 a las 22:47:59
 -- Versión del servidor: 10.11.13-MariaDB-0ubuntu0.24.04.1
 -- Versión de PHP: 8.3.6
 
@@ -84,7 +84,23 @@ INSERT INTO `fechas_horas` (`id_fechas_horas`, `fecha`, `hora`) VALUES
 (4, '2025-11-20', '19:50:00'),
 (5, '2025-11-04', '21:07:00'),
 (6, '2025-11-09', '19:07:00'),
-(7, '2025-11-14', '20:30:00');
+(7, '2025-11-14', '20:30:00'),
+(8, '2025-11-14', '12:30:00'),
+(9, '2025-11-30', '12:10:00'),
+(10, '2025-11-11', '12:24:00'),
+(11, '2025-11-11', '19:50:00'),
+(12, '2025-11-11', '16:56:00'),
+(13, '2025-11-12', '13:22:00'),
+(14, '2025-11-12', '13:00:00'),
+(15, '2025-11-28', '19:29:00'),
+(16, '2025-11-23', '19:32:00'),
+(17, '2025-11-30', '21:35:00'),
+(18, '2025-11-20', '20:40:00'),
+(19, '2025-11-18', '22:45:00'),
+(20, '2025-11-17', '20:55:00'),
+(21, '2025-11-17', '22:08:00'),
+(22, '2025-11-30', '18:55:00'),
+(23, '2025-12-17', '21:22:00');
 
 -- --------------------------------------------------------
 
@@ -123,14 +139,11 @@ CREATE TABLE `pago_sesiones` (
 --
 
 INSERT INTO `pago_sesiones` (`fk_metodos_pago`, `fk_sesiones`) VALUES
-(1, 10),
-(1, 13),
-(1, 15),
-(1, 16),
-(2, 10),
-(3, 13),
-(3, 15),
-(3, 16);
+(1, 26),
+(1, 27),
+(3, 26),
+(3, 27),
+(4, 27);
 
 -- --------------------------------------------------------
 
@@ -156,8 +169,8 @@ CREATE TABLE `personas` (
 INSERT INTO `personas` (`id_personas`, `nombre`, `apellido`, `dni`, `fecha_nacimiento`, `telefono`, `email`, `fk_rol`) VALUES
 (1, 'marcos', 'di filippo', 1111, '2007-06-26', 3525, 'admin@gmail.com', 1),
 (2, 'juan', 'perez', 2222, '2005-06-26', 3525555, 'secretario@gmail.com', 2),
-(3, 'beniciooooo', 'mercanteeeeee', 14151421, '2006-02-02', 15151242, 'benicio@gmail.com', 3),
-(9, 'galo', 'rose', 13511241, '2025-11-07', 6311513, 'galo@gmail.com', 3);
+(13, 'marcos', 'di filippo', 47911625, '2007-06-26', 321453111, 'marcos@gmail.com', 3),
+(14, 'marcos', 'difi 2', 7813241, '2010-06-19', 6253115, 'marcos2@gmail.com', 3);
 
 -- --------------------------------------------------------
 
@@ -200,8 +213,8 @@ CREATE TABLE `sesiones` (
 --
 
 INSERT INTO `sesiones` (`id_sesiones`, `detalles`, `imagen`, `fk_personas`, `fk_fechas_horas`, `fk_estado_sesion`, `monto`) VALUES
-(10, 'a', '1762164759.jpg', 3, 1, 3, 22222),
-(16, 'gal rose', '1762547412.jpg', 9, 7, 3, 45000);
+(26, 'sesion de marcos di filippo', '1763419164.webp', 13, 22, 3, 56000),
+(27, 'marcos difi 2 sesion nuevaaaa', '1763419499.webp', 14, 23, 3, 45000);
 
 -- --------------------------------------------------------
 
@@ -219,18 +232,13 @@ CREATE TABLE `sesiones_tratamientos` (
 --
 
 INSERT INTO `sesiones_tratamientos` (`fk_sesiones`, `fk_tratamientos`) VALUES
-(10, 1),
-(10, 2),
-(10, 3),
-(13, 1),
-(13, 3),
-(13, 5),
-(15, 5),
-(15, 6),
-(15, 9),
-(16, 1),
-(16, 3),
-(16, 7);
+(26, 2),
+(26, 8),
+(27, 1),
+(27, 3),
+(27, 5),
+(27, 8),
+(27, 11);
 
 -- --------------------------------------------------------
 
@@ -256,7 +264,8 @@ INSERT INTO `tratamientos` (`id_tratamientos`, `nombre`) VALUES
 (6, 'Fisioterapia'),
 (7, 'Kinesiologia deportiva'),
 (8, 'Crioterapia'),
-(9, 'Termoterapia');
+(9, 'Termoterapia'),
+(11, 'oculista');
 
 --
 -- Índices para tablas volcadas
@@ -350,7 +359,7 @@ ALTER TABLE `estados_sesiones`
 -- AUTO_INCREMENT de la tabla `fechas_horas`
 --
 ALTER TABLE `fechas_horas`
-  MODIFY `id_fechas_horas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_fechas_horas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `metodos_pago`
@@ -362,7 +371,7 @@ ALTER TABLE `metodos_pago`
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id_personas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_personas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -374,13 +383,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `sesiones`
 --
 ALTER TABLE `sesiones`
-  MODIFY `id_sesiones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_sesiones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `tratamientos`
 --
 ALTER TABLE `tratamientos`
-  MODIFY `id_tratamientos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_tratamientos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
