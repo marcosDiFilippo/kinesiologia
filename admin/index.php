0e8c3641cc0ef165fffa2c36f3f4f758f28b5e71
marcosDiFilippo <marcosjesusdifilippo@gmail.com>
2025-11-28 09:17:56 -0300

    mejoramos los estilos del login

---
+++ admin/index.php
@@ -0,0 +1,41 @@
+<?php
+    $seccion = "Inicio";
+    include_once("../componentes-admin/header.php");
+?>
+    <main>
+        <section class="section-categorias container-fluid d-flex flex-column">
+            <article class="row article-links-1">
+                <?php
+                    if ($esAdmin == true) {
+                        echo "<a href='administradores.php' class='col-6 d-flex flex-column justify-content-center align-items-center'>
+                            <svg class='svg-1' xmlns='http://www.w3.org/2000/svg' width='200' height='200' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='icon icon-tabler icons-tabler-outline icon-tabler-lock-minus'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M12.5 21h-5.5a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2' /><path d='M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0' /><path d='M8 11v-4a4 4 0 1 1 8 0v4' /><path d='M16 19h6' /></svg>
+                            <span class='span-index'>Administradores</span>
+                        </a>";
+                    }
+                    else {
+                        echo "<a href='informacion-general.php' class='col-6 d-flex flex-column justify-content-center align-items-center'>
+                            <svg class='svg-1' xmlns='http://www.w3.org/2000/svg' width='200' height='200' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='icon icon-tabler icons-tabler-outline icon-tabler-notes'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z' /><path d='M9 7l6 0' /><path d='M9 11l6 0' /><path d='M9 15l4 0' /></svg>
+                            <span class='span-index'>Ver informacion general</span>
+                        </a>";
+                    }
+                ?>
+                <a href="pacientes.php" class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <svg class="svg-2" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                    <span class="span-index">Pacientes</span>
+                </a>
+            </article>
+            <article class="row article-links-2">
+                <a href="sesiones.php" class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <svg class="svg-2" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-week"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M7 14h.013" /><path d="M10.01 14h.005" /><path d="M13.01 14h.005" /><path d="M16.015 14h.005" /><path d="M13.015 17h.005" /><path d="M7.01 17h.005" /><path d="M10.01 17h.005" /></svg>
+                    <span class="span-index">Sesiones</span>
+                </a>
+                <a href="tratamientos.php" class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <svg class="svg-1" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-stretching"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M5 20l5 -.5l1 -2" /><path d="M18 20v-5h-5.5l2.5 -6.5l-5.5 1l1.5 2" /></svg>
+                    <span class="span-index">Tratamientos</span>
+                </a>
+            </article>
+        </section>
+    </main>
+<?php
+    include_once("../componentes-admin/footer.php");
+?>
