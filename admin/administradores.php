543ffa61f96a89be56103df76ab33f1ff3c16193
Marcos Di Filippo <marcosjesusdifilippo@gmail.com>
2025-12-09 08:24:19 -0300

    Update administradores.php

--- admin/administradores.php
+++ admin/administradores.php
@@ -25,20 +25,27 @@
                     <th>
                         Fecha Nacimiento
                     </th>
+                    <th>
+                        Permisos
+                    </th>
                 </thead>
                 <tbody>
                     <?php
                     $lecturaUsuarios .= " WHERE `fk_rol`!=3";
                     $admins = mysqli_query($conexion, $lecturaUsuarios);
                         while ($admin = mysqli_fetch_array($admins)) {
+                            $lecturaRoles = "SELECT * FROM `roles`";
+                            if ($rolUsuario = mysqli_fetch_array(mysqli_query($conexion, $lecturaRoles .= " WHERE `id_rol`=$admin[fk_rol]"))) {
                                 echo "<tr>";
                                 echo "<td class='columna-nombre'>$admin[nombre] $admin[apellido]</td>
                                 <td>$admin[email]</td>
                                 <td>$admin[telefono]</td>
                                 <td>$admin[dni]</td>
-                                <td>$admin[fecha_nacimiento]</td>";
+                                <td>$admin[fecha_nacimiento]</td>
+                                <td>$rolUsuario[nombre]</td>";
                                 echo "</tr>";
                             }
+                        }
                     ?>
                 </tbody>
             </table>
